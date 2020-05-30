275d8f6bdd429a8be0267270fd9c9dfaef8fdeee
AlanGuo <AlanGuo>
2020-05-31 02:45:37 +0800

    update

--- pages/manage/token/new.tsx
+++ pages/manage/token/new.tsx
@@ -97,7 +97,8 @@ class NewToken extends React.Component<IProps, IState> {
                 autoFocus
                 onChange={(evt) => { this.setState({ symbol: evt.target.value.trim() }); }}
                 value={symbol}
-                maxLength={6}
+                minLength={2}
+                maxLength={16}
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 type="text"
                 placeholder="Symbol"/>
