f2c09d7c80f4694a44a6d855920318ef1eb52600
hffp <510033939@qq.com>
2020-08-03 10:04:30 +0800

    bug fix

--- pages/manage/recommend/index.tsx
+++ pages/manage/recommend/index.tsx
@@ -1,23 +1,23 @@
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { WithTranslation } from "next-i18next";
+import {faCheckCircle} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {WithTranslation} from "next-i18next";
 import Router from "next/router";
 import React from "react";
 import classnames from "classnames";
-import { connect } from "react-redux";
-import { bindActionCreators, Dispatch } from "redux";
+import {connect} from "react-redux";
+import {bindActionCreators, Dispatch} from "redux";
 import FrameLayout from "../../../components/FrameLayout";
 import Tips from "../../../components/Tips";
-import { withTranslation } from "../../../i18n";
+import {withTranslation} from "../../../i18n";
 import {
   closeAlert,
   setWallet,
   showErrorMessage,
   showSuccessMessage,
 } from "../../../store/actions";
-import { getAxios } from "../../../utils/axios";
-import { ACTIONS, API_URL, CHAIN_URL, CONTRACT_ADDRESS, SERVER_API_URL, STATUS } from "../../../utils/constant";
-import { chainErrorMessage } from "../../../utils/helper";
+import {getAxios} from "../../../utils/axios";
+import {ACTIONS, API_URL, CHAIN_URL, CONTRACT_ADDRESS, SERVER_API_URL, STATUS} from "../../../utils/constant";
+import {chainErrorMessage} from "../../../utils/helper";
 
 interface IProps extends WithTranslation {
   stores: [any];
@@ -42,13 +42,13 @@ class Index extends React.Component<IProps> {
 
   public static async getInitialProps(ctx) {
     const isServer = !!ctx.req;
-    const { dispatch } = ctx.store;
+    const {dispatch} = ctx.store;
     dispatch({type: ACTIONS.BUSY});
     const res = await getAxios(ctx).get(`${ isServer ? SERVER_API_URL : API_URL }/cms/recommends`);
     const stores = res.data.data;
     // By returning { props: posts }, the Blog component
     // will receive `posts` as a prop at build time
-    dispatch({ type: ACTIONS.FREE });
+    dispatch({type: ACTIONS.FREE});
     return {
       namespacesRequired: ["common"],
       stores,
@@ -71,50 +71,61 @@ class Index extends React.Component<IProps> {
   public render() {
     const {stores} = this.state;
     const grid = <table className="table-auto w-full">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">店铺</th>
-            <th className="px-4 py-2">封面</th>
-            <th className="px-4 py-2">代币</th>
-            <th className="px-4 py-2">推荐</th>
-            <th className="px-4 py-2">操作</th>
-          </tr>
-        </thead>
-        <tbody>
-        { stores.map((item: any, index) => {
+      <thead>
+      <tr>
+        <th className="px-4 py-2">店铺</th>
+        <th className="px-4 py-2">封面</th>
+        <th className="px-4 py-2">代币</th>
+        <th className="px-4 py-2">推荐</th>
+        <th className="px-4 py-2">操作</th>
+      </tr>
+      </thead>
+      <tbody>
+      {stores.map((item: any, index) => {
         return <tr key={index}>
           <td className="border px-4 py-2 text-center">
-            { item.name }
+            {item.name}
           </td>
           <td className="border px-4 py-2">
-          <div className="text-center">
-            { item.imgs.map((it, id) => (
-              <img className="mr-1 inline-block" width="80" key={id} src={it}/>
-            )) }
-          </div>
+            <div className="text-center">
+              {item.imgs.map((it, id) => (
+                <img className="mr-1 inline-block" width="80" key={id} src={it}/>
+              ))}
+            </div>
           </td>
           <td className="border px-4 py-2 text-center">
-            <a onClick={() => { Router.push(`/manage/token`); }} className="text-blue-500 cursor-hand">
-            { item.token[0] ? item.token[0].symbol : "" }
+            <a onClick={() => {
+              Router.push(`/manage/token`);
+            }} className="text-blue-500 cursor-hand">
+              {item.token[0] ? item.token[0].symbol : ""}
             </a>
           </td>
           <td className="border px-4 py-2 text-green-500">
             <div className="text-center">
-              { item.recommend &&
-              <FontAwesomeIcon className="w-8 inline-block" icon={faCheckCircle}></FontAwesomeIcon> }
+              {item.recommend &&
+              <FontAwesomeIcon className="w-8 inline-block" icon={faCheckCircle}></FontAwesomeIcon>}
             </div>
           </td>
           <td className="border px-4 py-2 text-center">
-            <button className={classnames("bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded ml-2 hover:border-transparent hover:text-white hover:bg-blue-500")}
-              onClick={ () => {
+            <button
+              className={classnames("bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded ml-2 hover:border-transparent hover:text-white hover:bg-blue-500")}
+              onClick={() => {
                 this.recommend(item);
-              } }>
-              { (item.recommend ? "取消" : "") + "推荐" }
+              }}>
+              {(item.recommend ? "取消" : "") + "推荐"}
             </button>
+            {!item.recommend ? <button
+              className={classnames("bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded ml-2 hover:border-transparent hover:text-white hover:bg-blue-500")}
+              onClick={() => {
+                this.recommend(item);
+              }}>
+              定时推荐
+            </button> : ""}
           </td>
-        </tr>; })
-        }
-        </tbody>
+        </tr>;
+      })
+      }
+      </tbody>
     </table>;
     const emptyGrid = <div className="mt-4 bg-gray-100 border px-4 py-2 text-center text-gray-800 text-sm">
       暂无店铺
@@ -123,7 +134,7 @@ class Index extends React.Component<IProps> {
       <FrameLayout>
         <Tips/>
         <div className="p-6">
-          { stores.length ? grid : emptyGrid }
+          {stores.length ? grid : emptyGrid}
         </div>
       </FrameLayout>
     );
@@ -133,7 +144,7 @@ class Index extends React.Component<IProps> {
     const timeInterval = setInterval(() => {
       const win = window as any;
       if (win.IWalletJS) {
-          win.IWalletJS.enable().then((account) => {
+        win.IWalletJS.enable().then((account) => {
           if (account) {
             clearInterval(timeInterval);
             this.props.setWallet(account);
@@ -176,8 +187,8 @@ function mapDispatchToProps(dispatch: Dispatch<any>) {
 }
 
 function mapStateToProps(state: any) {
-  const { wallet, errorMessage, showError, showSuccess, successMessage, isLoading } = state;
-  return { wallet, errorMessage, showError, showSuccess, successMessage, isLoading };
+  const {wallet, errorMessage, showError, showSuccess, successMessage, isLoading} = state;
+  return {wallet, errorMessage, showError, showSuccess, successMessage, isLoading};
 }
 
 export default connect(
