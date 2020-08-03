4873d6859ffb0d9ffb75dbb15e5d2ed75bb095bd
hffp <510033939@qq.com>
2020-08-03 15:12:08 +0800

    update

--- pages/manage/recommend/index.tsx
+++ pages/manage/recommend/index.tsx
@@ -172,6 +172,7 @@ class Index extends React.Component<IProps> {
               <label className="block text-gray-700 text-sm font-bold mb-2">
                 库存
               </label>
+              开始时间:
               <input
                 onChange={
                   (evt) => {
@@ -179,7 +180,8 @@ class Index extends React.Component<IProps> {
                   }
                 }
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="username" type="datetime-local" min="0" placeholder="开始时间"/>
+                id="startTime" type="datetime-local" min="0" placeholder="开始时间"/>
+              结束时间:
               <input
                 onChange={
                   (evt) => {
@@ -187,7 +189,7 @@ class Index extends React.Component<IProps> {
                   }
                 }
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="username" type="datetime-local" min="0" placeholder="结束时间"/>
+                id="endTime" type="datetime-local" min="0" placeholder="结束时间"/>
             </div>
             <div className="flex items-center justify-between">
               <button
