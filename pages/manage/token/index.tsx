c7a00ac109eae477be9d44260fdec5a289420d36
hffp <510033939@qq.com>
2020-08-10 16:21:41 +0800

    update

--- pages/manage/token/index.tsx
+++ pages/manage/token/index.tsx
@@ -128,7 +128,7 @@ class Index extends React.Component<IProps> {
           }
           {
             <td className="border px-4 py-2 text-center">
-              {item.repoBalance || 0}
+              {item.repoBalance || 0} IOST
               <button
                 className="bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded ml-2 hover:border-transparent hover:text-white hover:bg-blue-500"
                 onClick={() => this.setState({showRecharge: true})}>
